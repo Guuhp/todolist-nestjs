59038620986050a48e5b7d2d0384b1b41ab81672
Guuhp <lg645471@gmail.com>
2022-09-18 20:13:16 -0500

    connect database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { Task } from './task/entities/task.entity';
 import { TaskModule } from './task/task.module';
+import { User } from './user/entities/user.entity';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TaskModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 9001,
+      username: 'root',
+      password: 'admin',
+      database: 'todolist',
+      entities: [User, Task],
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+    TaskModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
